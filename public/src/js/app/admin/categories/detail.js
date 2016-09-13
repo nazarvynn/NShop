ce5e4b6b2066722d38364acccd4900ab13803b31
Nazar Vynnytskyi <nazar.vynn@gmail.com>
2016-09-13 21:55:26 +0300

    Added actions for categories

--- public/src/js/app/admin/categories/detail.js
+++ public/src/js/app/admin/categories/detail.js
@@ -1,9 +1,29 @@
 NShop.controller('AdminCategoriesDetail', function ($scope, $stateParams, CategoriesService) {
 
+    var actions_ = {
+        'create': function () {
+            console.log('create 1');
+        },
+        'edit': function (categoryId) {
+            console.log('edit 1');
+            CategoriesService.getCategory(categoryId).then(function (data) {
+                $scope.category = data;
+            });
+        }
+    };
+
+    $scope.remove = function (categoryId) {
+        applyAction('remove', categoryId);
+    };
+
+    function applyAction(type, categoryId) {
+        actions_[type](categoryId);
+    }
+
     function init_() {
-        CategoriesService.getCategory($stateParams.categoryId).then(function (data) {
-            $scope.category = data;
-        });
+        var categoryId = $stateParams.categoryId;
+        var action  = 'create' === categoryId ? 'create' : 'edit';
+        applyAction(action, categoryId);
     }
 
     init_();
--- public/src/js/app/admin/categories/list.js
+++ public/src/js/app/admin/categories/list.js
@@ -1,5 +1,16 @@
 NShop.controller('AdminCategoriesList', function ($scope, CategoriesService) {
 
+    $scope.remove = function (categoryId) {
+        console.log('delete 1', categoryId);
+        //var txt;
+        //var r = confirm("Press a button!");
+        //if (r == true) {
+        //    txt = "You pressed OK!";
+        //} else {
+        //    txt = "You pressed Cancel!";
+        //}
+    };
+
     function init_() {
         $scope.activeItem = 'categories';
 
